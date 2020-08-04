85cda90aa8c3e6c9cb693b99a63ac676c3ffdc67
Andrei Todorov <andrei@teamgeek.io>
2020-08-04 06:19:40 +0200

    basic extension structure and first test

--- src/flask_odoo/__init__.py
+++ src/flask_odoo/__init__.py
@@ -1,5 +1,63 @@
 import logging
+import xmlrpc.client
+
+from flask import _app_ctx_stack, current_app
 
 __version__ = "0.0.1"
 
 logger = logging.getLogger(__name__)
+
+
+class Odoo:
+    """Stores Odoo XML RPC server proxies and authentication information
+    inside Flask's application context.
+
+    """
+
+    def __init__(self, app=None):
+        self.app = app
+        if self.app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.teardown_appcontext(self.teardown)
+
+    def teardown(self, exception):
+        ctx = _app_ctx_stack.top
+        if hasattr(ctx, "odoo_common"):
+            delattr(ctx, "odoo_common")
+        if hasattr(ctx, "odoo_uid"):
+            delattr(ctx, "odoo_uid")
+
+    def create_common(self):
+        ODOO_URL = current_app.config["ODOO_URL"]
+        common = xmlrpc.client.ServerProxy(
+            "{}/xmlrpc/2/common".format(ODOO_URL)
+        )
+        return common
+
+    @property
+    def common(self):
+        ctx = _app_ctx_stack.top
+        if ctx is not None:
+            if not hasattr(ctx, "odoo_common"):
+                ctx.odoo_common = self.create_common()
+            return ctx.odoo_common
+
+    def authenticate(self):
+        """Returns a user identifier (uid) used in authenticated calls."""
+        ODOO_DB = current_app.config["ODOO_DB"]
+        ODOO_USERNAME = current_app.config["ODOO_USERNAME"]
+        ODOO_PASSWORD = current_app.config["ODOO_PASSWORD"]
+        uid = self.common.authenticate(
+            ODOO_DB, ODOO_USERNAME, ODOO_PASSWORD, {}
+        )
+        return uid
+
+    @property
+    def uid(self):
+        ctx = _app_ctx_stack.top
+        if ctx is not None:
+            if not hasattr(ctx, "odoo_uid"):
+                ctx.odoo_uid = self.authenticate()
+            return ctx.odoo_common
--- tests/test_flask_odoo.py
+++ tests/test_flask_odoo.py
@@ -1,3 +1,22 @@
+from flask import Flask
 
-def test_foo():
-    assert True
+from flask_odoo import Odoo
+
+
+def test_init(mocker):
+    mocked_init_app = mocker.patch.object(Odoo, "init_app")
+
+    import_name = __name__.split(".")[0]
+    app = Flask(import_name)
+    app.config["ODOO_URL"] = "http://localhost:8069"
+    app.config["ODOO_DB"] = "odoo"
+    app.config["ODOO_USERNAME"] = "admin"
+    app.config["ODOO_PASSWORD"] = "admin"
+
+    odoo = Odoo(app)
+
+    assert odoo.app == app
+    with app.app_context() as app_context:
+        common = odoo.common
+        assert app_context.odoo_common == common
+    mocked_init_app.assert_called_with(app)
