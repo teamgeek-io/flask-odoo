f3a3f0a0343877f4cbd67a042e11714ca26e42bd
Andrei Todorov <andrei@teamgeek.io>
2020-08-12 13:43:08 +0200

    Add support for default domain on a model

---
+++ src/flask_odoo/model.py
@@ -0,0 +1,84 @@
+import schematics
+
+
+def _model_name(cls):
+    return cls._name or cls.__name__.lower()
+
+
+def _construct_domain(cls, search_criteria=None):
+    domain = []
+    if cls._domain:
+        domain.extend(cls._domain)
+    if search_criteria:
+        domain.extend(search_criteria)
+    return domain
+
+
+def search_count(cls, search_criteria=None):
+    model_name = cls._model_name()
+    domain = cls._construct_domain(search_criteria)
+    return cls._odoo[model_name].search_count(domain)
+
+
+def search_read(cls, search_criteria=None, offset=None, limit=None):
+    model_name = cls._model_name()
+    domain = cls._construct_domain(search_criteria)
+    fields = [
+        field.serialized_name or name
+        for name, field in cls._schema.fields.items()
+    ]
+    kwargs = {"fields": fields}
+    if offset:
+        kwargs["offset"] = offset
+    if limit:
+        kwargs["limit"] = limit
+    records = cls._odoo[model_name].search_read(domain, **kwargs)
+    return [cls(rec) for rec in records]
+
+
+def search_by_id(cls, id):
+    search_criteria = cls._construct_domain([["id", "=", id]])
+    objects = cls.search_read(search_criteria, limit=1)
+    return objects[0] if objects else None
+
+
+def create_or_update(self):
+    model_name = self._model_name()
+    vals = self.to_primitive()
+    vals.pop("id", None)
+    if self.id:
+        self._odoo[model_name].write([self.id, vals])
+    else:
+        self.id = self._odoo[model_name].create(vals)
+
+
+def delete(self):
+    model_name = self._model_name()
+    if self.id:
+        self._odoo[model_name].unlink([self.id])
+
+
+def __repr__(self):
+    return f"<{self.__class__.__name__}(id={self.id})>"
+
+
+def make_model_base(odoo):
+    """Return a base class for Odoo models to inherit from."""
+    return type(
+        "BaseModel",
+        (schematics.models.Model,),
+        dict(
+            _odoo=odoo,
+            _name=None,
+            _domain=None,
+            id=schematics.types.IntType(),
+            _model_name=classmethod(_model_name),
+            _construct_domain=classmethod(_construct_domain),
+            search_count=classmethod(search_count),
+            search_read=classmethod(search_read),
+            search_by_id=classmethod(search_by_id),
+            create_or_update=create_or_update,
+            delete=delete,
+            __repr__=__repr__,
+        ),
+    )
--- tests/test_flask_odoo.py
+++ tests/test_flask_odoo.py
@@ -1,16 +1,15 @@
 from unittest.mock import MagicMock
 
-import schematics.models
-import schematics.types
-
 from flask_odoo import ObjectProxy, Odoo
 
 
 def test_odoo_init(app, mocker):
+    make_model_base_mock = mocker.patch("flask_odoo.make_model_base")
     init_app_mock = mocker.patch.object(Odoo, "init_app")
     odoo = Odoo(app)
     assert odoo.app == app
-    assert isinstance(odoo.Model, type)
+    make_model_base_mock.assert_called_with(odoo)
+    assert odoo.Model == make_model_base_mock.return_value
     init_app_mock.assert_called_with(app)
 
 
@@ -55,28 +54,14 @@ def test_odoo_object(app, app_context, mocker):
 
 
 def test_odoo_getitem(app, app_context):
+    odoo = Odoo(app)
     app_context.odoo_common = MagicMock()
     app_context.odoo_common.authenticate.return_value = 1
     app_context.odoo_object = MagicMock()
-    odoo = Odoo(app)
     object_proxy = odoo["test.model"]
     assert isinstance(object_proxy, ObjectProxy)
     object_proxy.odoo == odoo
     assert object_proxy.model_name == "test.model"
-
-
-def test_odoo_make_model_base(app, app_context):
-    odoo = Odoo(app)
-    Model = odoo.make_model_base()
-
-    class Partner(Model):
-        _name = "res.partner"
-
-    partner = Partner()
-
-    assert isinstance(partner, schematics.models.Model)
-    assert partner.odoo is odoo
-    assert partner._name == "res.partner"
 
 
 def test_object_proxy_init():
@@ -119,158 +104,3 @@ def test_object_proxy_method_call(app, app_context):
         ("arg1",),
         {"kwarg1": "test_kwarg"},
     )
-
-
-def test_base_model_no_name(app, app_context):
-    odoo = Odoo(app)
-    Model = odoo.make_model_base()
-
-    class Partner(Model):
-        pass
-
-    partner = Partner()
-    assert partner._model_name() == "partner"
-
-
-def test_base_model_search_count(app, app_context):
-    odoo = Odoo(app)
-    app_context.odoo_common = MagicMock()
-    app_context.odoo_common.authenticate.return_value = 1
-    app_context.odoo_object = MagicMock()
-    app_context.odoo_object.execute_kw.return_value = 2
-
-    class Partner(odoo.Model):
-        _name = "res.partner"
-
-    domain = [["is_company", "=", True]]
-    count = Partner.search_count(domain)
-
-    app_context.odoo_object.execute_kw.assert_called_with(
-        "odoo", 1, "admin", "res.partner", "search_count", (domain,), {}
-    )
-    assert count == 2
-
-
-def test_base_model_search_read(app, app_context):
-    odoo = Odoo(app)
-    app_context.odoo_common = MagicMock()
-    app_context.odoo_common.authenticate.return_value = 1
-    app_context.odoo_object = MagicMock()
-    records = [
-        {"id": 1, "name": "rec1", "active": True},
-        {"id": 2, "name": "rec2", "active": False},
-    ]
-    app_context.odoo_object.execute_kw.return_value = records
-
-    class Partner(odoo.Model):
-        _name = "res.partner"
-        name = schematics.types.StringType()
-        is_active = schematics.types.BooleanType(serialized_name="active")
-
-    domain = [["is_company", "=", True]]
-    objects = Partner.search_read(domain, offset=100, limit=10)
-    app_context.odoo_object.execute_kw.assert_called_with(
-        "odoo",
-        1,
-        "admin",
-        "res.partner",
-        "search_read",
-        (domain,),
-        {"fields": ["id", "name", "active"], "offset": 100, "limit": 10},
-    )
-    assert objects == [Partner(records[0]), Partner(records[1])]
-    assert objects[0].is_active
-    assert not objects[1].is_active
-
-
-def test_base_model_search_by_id(app, app_context):
-    odoo = Odoo(app)
-    app_context.odoo_common = MagicMock()
-    app_context.odoo_common.authenticate.return_value = 1
-    app_context.odoo_object = MagicMock()
-    app_context.odoo_object.execute_kw.return_value = [
-        {"id": 2, "name": "test_partner"}
-    ]
-
-    class Partner(odoo.Model):
-        _name = "res.partner"
-        name = schematics.types.StringType()
-
-    partner = Partner.search_by_id(2)
-    app_context.odoo_object.execute_kw.assert_called_with(
-        "odoo",
-        1,
-        "admin",
-        "res.partner",
-        "search_read",
-        ([["id", "=", 2]],),
-        {"fields": ["id", "name"], "limit": 1},
-    )
-    assert partner.id == 2
-    assert partner.name == "test_partner"
-
-
-def test_base_model_create_or_update(app, app_context):
-    odoo = Odoo(app)
-    app_context.odoo_common = MagicMock()
-    app_context.odoo_common.authenticate.return_value = 1
-    app_context.odoo_object = MagicMock()
-    app_context.odoo_object.execute_kw.return_value = 2
-
-    class Partner(odoo.Model):
-        _name = "res.partner"
-        name = schematics.types.StringType()
-
-    partner = Partner()
-    assert not partner.id
-    partner.name = "test_partner"
-    partner.create_or_update()
-    app_context.odoo_object.execute_kw.assert_called_with(
-        "odoo",
-        1,
-        "admin",
-        "res.partner",
-        "create",
-        ({"name": "test_partner"},),
-        {},
-    )
-    assert partner.id == 2
-    partner.name = "new_name"
-    partner.create_or_update()
-    app_context.odoo_object.execute_kw.assert_called_with(
-        "odoo",
-        1,
-        "admin",
-        "res.partner",
-        "write",
-        ([2, {"name": "new_name"}],),
-        {},
-    )
-
-
-def test_base_model_delete(app, app_context):
-    odoo = Odoo(app)
-    app_context.odoo_common = MagicMock()
-    app_context.odoo_common.authenticate.return_value = 1
-    app_context.odoo_object = MagicMock()
-
-    class Partner(odoo.Model):
-        _name = "res.partner"
-
-    partner = Partner()
-    partner.id = 2
-    partner.delete()
-    app_context.odoo_object.execute_kw.assert_called_with(
-        "odoo", 1, "admin", "res.partner", "unlink", ([2],), {}
-    )
-
-
-def test_base_model_repr(app):
-    odoo = Odoo(app)
-
-    class Partner(odoo.Model):
-        _name = "res.partner"
-
-    partner = Partner()
-    partner.id = 1
-    assert str(partner) == "<Partner(id=1)>"
